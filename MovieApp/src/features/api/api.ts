6638d227a74ff4ce4e58bc5ab93c5a1f2c484a0b
AlenaRaik <alena.raikevich@gmail.com>
2023-12-19 19:13:40 +0300

    add api

--- MovieApp/src/features/api/api.ts
+++ MovieApp/src/features/api/api.ts
@@ -1,8 +1,8 @@
 import { KinopoinskAllUnofficial, SearchSmall } from "#ui/cards/card-props";
 
 export const kinopoiskUnofficial = 'https://kinopoiskapiunofficial.tech/';
-export const X_API_KEY_Unofficial = 'f08acb7a-336b-47e7-82b9-651c3c8005ef';
-
+// export const X_API_KEY_Unofficial = 'f08acb7a-336b-47e7-82b9-651c3c8005ef';
+export const X_API_KEY_Unofficial = '923b524c-efdb-4e68-b1ab-72bcf18be01c';
 
 export const kinopoiskUrl = 'https://api.kinopoisk.dev/';
 export const X_API_KEY = 'MTSWC3Z-7PJM5NZ-HQ43FVP-9HCPJXE';
